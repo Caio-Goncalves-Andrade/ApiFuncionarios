a3b47995042bd141d69032aec705f71ca4d9ee1f
Your Name <you@example.com>
2020-10-05 19:03:32 -0300

    Primeiro

---
+++ CadastroFuncionários/Data/CadastroFuncionáriosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CadastroFuncionários;
+
+namespace CadastroFuncionários.Data
+{
+    public class CadastroFuncionáriosContext : DbContext
+    {
+        public CadastroFuncionáriosContext (DbContextOptions<CadastroFuncionáriosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroFuncionários.Funcionario> Funcionario { get; set; }
+    }
+}
---
+++ CadastroFuncionários/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroFuncionários
+{
+    public class Funcionario
+    {
+        public int? Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public DateTime Nascimento { get; set; }
+
+        public int Salario { get; set; }
+
+        public string Genero { get; set; }
+    }
+}
